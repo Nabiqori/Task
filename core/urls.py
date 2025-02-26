3e985b6307294b6cd3b399e946e50bc808eea500
Пользователь <nabiqorinabiyev0@gmail.com>
2025-02-26 19:42:16 +0500

    second

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import HomeView, EditView, DeleteView
+from main.views import HomeView, EditView, DeleteView, login_view, logout_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(),name="home"),
     path('<int:pk>/edit/', EditView.as_view(),name="edit"),
     path('<int:pk>/delete/', DeleteView.as_view(),name="delete"),
+
+]
+urlpatterns += [
+    path('login/', login_view,name="login"),
+    path('logout/', logout_view,name="logout"),
+    path('register/', register_view,name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,46 +1,94 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
+
+
 from .models import *
 
 class HomeView(View):
     def get(self, request):
-        tasks = Task.objects.exclude(status='done').order_by('-deadline')
-        context = {
-            "tasks":tasks,
-            "dones":Task.objects.filter(status='done'),
-        }
-        return render(request, 'index.html', context)
+        if request.user.is_authenticated:
+            tasks = Task.objects.filter(user=request.user).exclude(status='done').order_by('-deadline')
+            context = {
+                "tasks":tasks,
+                "dones":Task.objects.filter(status='done', user=request.user),
+            }
+            return render(request, 'index.html', context)
+        return redirect('login')
     def post(self,request):
-        Task.objects.create(
-            title=request.POST.get('title'),
-            details=request.POST.get('details'),
-            status=request.POST.get('status'),
-            deadline=request.POST.get('deadline'),
-            user=User.objects.last()
-        )
-        return redirect('home')
+        if request.user.is_authenticated:
+            if request.POST.get('deadline') == '':
+                deadline=None
+            else:
+                deadline=request.POST.get('deadline')
+            Task.objects.create(
+                title=request.POST.get('title'),
+                details=request.POST.get('details'),
+                status=request.POST.get('status'),
+                deadline=deadline,
+                user=request.user
+            )
+            return redirect('home')
+        return redirect("login")
 class EditView(View):
     def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        context = {
-            "task":task,
-        }
-        return render(request, "edit.html", context)
+        if request.user.is_authenticated:
+            task = get_object_or_404(Task, pk=pk, user=request.user)
+            context = {
+                "task":task,
+            }
+            return render(request, "edit.html", context)
+        return redirect('login')
     def post(self,request,pk):
-        Task.objects.filter(pk=pk).update(
-            title=request.POST.get('title'),
-            details=request.POST.get('details'),
-            status=request.POST.get('status'),
+        if request.user.is_authenticated:
+            Task.objects.filter(pk=pk, user=request.user).update(
+                title=request.POST.get('title'),
+                details=request.POST.get('details'),
+                status=request.POST.get('status'),
 
-        )
-        return redirect('home')
+            )
+            return redirect('home')
+        return redirect('login')
 class DeleteView(View):
     def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        context = {
-            "task": task,
-        }
-        return render(request, "delete.html", context)
+        if request.user.is_authenticated:
+            task = get_object_or_404(Task, pk=pk, user=request.user)
+            context = {
+                "task": task,
+            }
+            return render(request, "delete.html", context)
+        return redirect('login')
     def post(self,request,pk):
-        Task.objects.filter(pk=pk).delete()
-        return redirect('home')
+        if request.user.is_authenticated:
+            Task.objects.filter(pk=pk, user=request.user).delete()
+            return redirect('home')
+        redirect('login')
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST.get('username'),
+            password=request.POST.get('password'),
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        return redirect("login")
+    return render(request, "login.html")
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('login')
+def register_view(request):
+    if request.method == 'POST':
+        if request.POST.get('password1') != request.POST.get('password2') or request.POST.get('username') in User.objects.values_list('username', flat=True):
+            return redirect("register")
+        user = User.objects.create_user(
+            username=request.POST.get('username'),
+            password=request.POST.get('password1'),
+        )
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        return redirect('register')
+    return render(request, "register.html")
